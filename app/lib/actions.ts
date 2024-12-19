3587785689854fc0f731673579bca43d61da9f79
hvnguyyen <nguyen.hoangvinh@outlook.com>
2024-12-19 11:21:17 +0100

    handling errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -21,11 +21,11 @@ export async function deleteInvoice(id: string): Promise<void> {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
     }catch(error){
-        console.error('Database Error: Failed to Delete Invoice');
+        console.log('Database Error: Failed to Delete Invoice' );
     }
 }
 
-export async function createInvoice(formData: FormData) {
+export async function createInvoice(formData: FormData): Promise<void> {
     const { customerId, amount, status } = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
@@ -39,16 +39,14 @@ export async function createInvoice(formData: FormData) {
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
-    }catch(error) {
-        return {
-            message: 'Database Error: Failed to Create Invoice',
-        };
-    }
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
-}
+    }catch(error) {
+        console.log('Database Error: Failed to Create Invoice');
+        }
+    }
 
-export async function updateInvoice(id: string, formData: FormData) {
+export async function updateInvoice(id: string, formData: FormData): Promise<void> {
     const { customerId, amount, status } = UpdateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
@@ -62,12 +60,9 @@ export async function updateInvoice(id: string, formData: FormData) {
         SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
         WHERE id = ${id}
     `;
-
-    }catch(error){
-        return {
-            message: 'Database Error: Failes to Update Invoice',
-        };
-    }
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
+    }catch(error){
+        console.log('Database Error: Failes to Update Invoice');
+    }
 }
